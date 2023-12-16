46e68b84d1f638c95655b3dec3ea13208c32a8d9
quentinRolld <quantinrld@gmail.com>
2023-12-16 18:16:21 +0100

    on met tout ça au propre

---
+++ generalization/adv_attack.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.datasets import fetch_openml
+import pandas as pd
+from matplotlib.colors import LinearSegmentedColormap
+
+""" Here is the function that will compute the adversarial examples """
+
+def adv_attack(model, image, epsilon, labels,criterion):
+
+    # Move the model to the CPU
+    image = image.clone().detach().requires_grad_(True)  # Create a new tensor with requires_grad set to True
+    labels = labels
+    sorties = model(image)
+
+    model.zero_grad()  # zero all the gradients
+    cost = criterion(sorties, labels)
+    cost.backward()  # compute the gradient of the loss
+
+    # compute the sign of gradient
+    sign_data_grad = image.grad.sign()
+    # generate the altered image
+    altered_image = image + epsilon * sign_data_grad
+
+    # clipping to maintain [0, 1] range
+    altered_image = torch.clamp(altered_image, 0, 1)
+
+    return altered_image
+
+def create_adv_test(model, X_test_tensor, y_test_tensor,eps,criterion):
+    adv_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+    adv_loader = DataLoader(adv_dataset, batch_size=len(adv_dataset), shuffle=False)
+    for batch_X, batch_y in adv_loader:
+        alt_im=adv_attack(model, batch_X, eps, batch_y,criterion)
+    alt_im_norm =torch.tensor(alt_im, dtype=torch.float32)
+    return alt_im_norm
